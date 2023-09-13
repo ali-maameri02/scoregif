0b9cd4ec392f4aa26d16206d2a23559903946365
unknown <ali.maameri@univ-constantine2.dz>
2023-09-13 01:44:50 +0100

    railway commit

--- football_predictions/settings.py
+++ football_predictions/settings.py
@@ -25,14 +25,17 @@ SECRET_KEY = 'django-insecure-xk1xph-nmesq=&^y9l6c1uj=&s$3hlo%0krgvw)=a!*7^v38ab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["127.0.0.1", ".railway.app"]
 TWITTER_API_KEY = 'lD8MIDfbsO216w7Jh04Rzt9x9'
 TWITTER_API_SECRET = 'Vje3Y4mmmMJltgc7YGGIYDGvUgzUswv91CL4kCGxXuHR2XRoOS'
 ACCESS_TOKEN = '1697935934929485824-w7uBXn8EfOp43BH6xQodGuA4jByRQn'
 ACCESS_TOKEN_SECRET = 'CL0MKtCDt7bGl8nsoYbYzEw4dzJ6prqgTDp3CWX9NzCEc'
 TWITTER_CALLBACK_URL = 'https://footballpredictions.local:8000/twitter/callback'
+import dj_database_url
+from django.conf import settings
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+settings.DATABASES["default"].update(db_from_env)
 
 # Application definition
 
