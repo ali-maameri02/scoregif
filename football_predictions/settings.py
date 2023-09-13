fd71ed50d1741ffe520e57cf71288699bfb9e1b3
unknown <ali.maameri@univ-constantine2.dz>
2023-09-13 23:25:04 +0100

    render commit

--- football_predictions/settings.py
+++ football_predictions/settings.py
@@ -98,18 +98,12 @@ WSGI_APPLICATION = 'football_predictions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'football_predictions',
-        'USER': 'root',
-        'PASSWORD': '40603010',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
