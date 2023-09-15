97ffa7f6a7baf248f9b3667b77620c949198a945
unknown <ali.maameri@univ-constantine2.dz>
2023-09-15 21:26:29 +0100

    render commit

--- predictions/admin.py
+++ predictions/admin.py
@@ -25,10 +25,20 @@ class TeamAdmin(admin.ModelAdmin):
 
         super().save_model(request, obj, form, change)
 
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_logo')
 
+    def display_logo(self, obj):
+        if obj.logo:
+            return format_html('<img src="{}" width="50" height="50" />', obj.logo.url)
+        else:
+            return "No Logo"
+
+    display_logo.short_description = 'Logo'
+
+admin.site.register(Tournament, TournamentAdmin)
 
 admin.site.register(Team, TeamAdmin)
-admin.site.register(Tournament)
 admin.site.register(Match)
 admin.site.register(Prediction)
 
