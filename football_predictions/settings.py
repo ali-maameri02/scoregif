919126488cd1fa635bf2850f2f6e9d81f64d0315
unknown <ali.maameri@univ-constantine2.dz>
2023-09-16 18:38:13 +0100

    render commit

--- football_predictions/settings.py
+++ football_predictions/settings.py
@@ -164,7 +164,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'football_predictions/static')
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
